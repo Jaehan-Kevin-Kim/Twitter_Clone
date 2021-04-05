5f0995d8206ce4cd48f8e91d1e442f21d3710c88
Kevin Kim <jhkevin21@gmail.com>
2021-04-04 19:28:01 -0600

    Add and get collection by using firestore.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,64 @@
-import React from 'react';
+import { dbService } from 'fBase';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
-  return <span>Home</span>;
+  const [tweet, setTweet] = useState('');
+  const [tweets, setTweets] = useState([]);
+  const getTweets = async () => {
+    const dbTweets = await dbService.collection('tweets').get();
+    console.log(dbTweets);
+    // dbTweets.forEach((document) => console.log(document.data()));
+    dbTweets.forEach((document) => {
+      const tweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setTweets((prev) => [tweetObject, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getTweets();
+  }, []);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection('tweets').add({
+      tweet: tweet,
+      createdAt: Date.now(),
+    });
+    setTweet('');
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+
+  console.log(tweets);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          onChange={onChange}
+          type='text'
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type='submit' value='Tweet' />
+      </form>
+      <div>
+        {tweets.map((tweet) => (
+          <div key={tweet.id}>
+            <h4>{tweet.tweet}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Home;
