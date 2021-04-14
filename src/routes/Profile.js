9049c99a9334bc3ed3409cc4e485124139847385
Kevin Kim <jhkevin21@gmail.com>
2021-04-14 11:17:51 -0600

    Update Profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,31 +1,59 @@
 import { authService, dbService } from 'fBase';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 const Profile = ({ userObj }) => {
   let history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
 
   const onLogOutClick = () => {
     authService.signOut();
     history.push('/');
   };
 
-  const getMyTweets = async () => {
-    //filter링 하는법 (profile화면에 해당 log in 한 user 정보를 표시하기 위해 해당 log in한 user가 맞는지 확인 해주는 기능)
-    const tweets = await dbService
-      .collection('tweets')
-      .where('creatorId', '==', userObj.uid)
-      .orderBy('createdAt')
-      .get();
+  // const getMyTweets = async () => {
+  //   //filter링 하는법 (profile화면에 해당 log in 한 user 정보를 표시하기 위해 해당 log in한 user가 맞는지 확인 해주는 기능)
+  //   const tweets = await dbService
+  //     .collection('tweets')
+  //     .where('creatorId', '==', userObj.uid)
+  //     .orderBy('createdAt')
+  //     .get();
 
-    console.log(tweets.docs.map((doc) => doc.data()));
+  //   console.log(tweets.docs.map((doc) => doc.data()));
+  // };
+  // useEffect(() => {
+  //   getMyTweets();
+  // }, []);
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
   };
-  useEffect(() => {
-    getMyTweets();
-  }, []);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      // console.log(userObj.updateProfile);
+      console.log(newDisplayName);
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+  };
 
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type='text'
+          placeholder='Display Name'
+          value={newDisplayName}
+        />
+        <input type='submit' value='Update Profile' />
+      </form>
       <button onClick={onLogOutClick}>Log out</button>
     </>
   );
